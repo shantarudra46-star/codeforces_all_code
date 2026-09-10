77173e7bdc111fdecf8cf84b9688642c78180894
Shanto Rudra <shantarudra46@gmail.com>
2026-09-10 23:03:17 +0600

    added the code

---
+++ Three_Indices.cpp
@@ -0,0 +1,19 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main () {
+
+    int t;
+    cin >> t;
+    while(t--) {
+        int n;
+        cin >> n;
+        int s[n];
+        for (int i=0;i<n;i++) {
+            cin >> s[i];
+        }
+        for (int i=0;i<n;i++) {
+            
+        }
+    }
+    return 0;
+}
