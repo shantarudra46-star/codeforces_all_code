720e14c36dbd606545a0e476d20b4ee459e3a5e5
Shanto Rudra <shantarudra46@gmail.com>
2026-09-12 21:59:11 +0600

    final code submit

--- Mocha-and-Math.cpp
+++ Mocha-and-Math.cpp
@@ -1,6 +1,22 @@
 #include<bits/stdc++.h>
 using namespace std;
 int main(){
-    
+    int t;
+    cin >> t;
+    while(t--) {
+        int n;
+        cin >> n;
+        int s[n];
+        
+        for (int i=0;i<n;i++) {
+            cin >> s[i];
+        }
+        int x=s[0];
+        for (int i=1;i<n;i++) {
+            x=x&s[i];
+        }    
+           
+        cout << x << endl;
+    }
     return 0;
 }
