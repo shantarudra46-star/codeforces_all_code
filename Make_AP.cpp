43a6d96b00f1ccbad53d86be9b5f5c4cf72f1273
Shanto Rudra <shantarudra46@gmail.com>
2026-09-14 22:41:50 +0600

    Submit final code

--- Make_AP.cpp
+++ Make_AP.cpp
@@ -1,6 +1,22 @@
 #include<bits/stdc++.h>
 using namespace std;
 int main () {
+    int t;
+    cin >> t;
+    while(t--) {
+        int a,b,c;
+        cin >> a >> b >> c;
 
+       if((2*b-c)>0 && (2*b-c)%a==0) {
+           cout << "YES" << endl;
+       }else if ((a+c)%(2*b)==0) {
+           cout << "YES" << endl;
+       }else if((2*b-a)>0 && (2*b-a)%c==0) {
+           cout << "YES" << endl;
+       } else {
+           cout << "NO" << endl;
+       }
+    }
     return 0;
+
 }
